83725e6f36a970f2d2180363bda6a82f9603ae06
Anthony Ma <tonyma_0706@163.com>
2020-06-03 22:00:55 +0800

    Add new functions for plot and data_transformation
    * add plot_timeseries function that take a single tf character input and activity dataset
    * add function (identify_tf) to make a dataframe with identity of tf (ext or regular type)
    * based on the function above, can generate a tf character with orgianal ext and weights attached on it using a clean tf input ("Arx") to grab data later

--- functions.R
+++ functions.R
@@ -14,7 +14,7 @@ plotByValue <- function(tf, cluster_data){
 }
 # generate a tibble that has two columns indicating whether the tf has ext type,
 # the ext column labels whether that data is ext type
-TF_and_ext <- function(TF_name_activity_tibble){
+identify_tf <- function(TF_name_activity_tibble){
   TF_name_activity_tibble %>% 
     rename(name = value) %>%
     mutate(no_space = str_replace_all(name, " \\(.+\\)$", ""))%>% #remove space and the gram
@@ -23,7 +23,7 @@ TF_and_ext <- function(TF_name_activity_tibble){
     mutate(type = str_replace(TF_type, "_ext", "")) %>%
     select(name, type, ext)
 }
-# three booleans that determine the quality of TF data in the activity datasets
+# boolean functions that determine the identity of TF data in the activity datasets
 has_regular <- function(TF, TF_ext_data){
   has_regular <- filter(TF_ext_data, type==TF & ext==TF)
   nrow(has_regular)!=0 #boolean
@@ -43,7 +43,7 @@ tf_exist <- function(TF){
   return (TRUE)
   #!has_regular(TF, TF_ext_data) && !has_ext(TF, TF_ext_data)
 }
-
+# read the corresponding data by tf's identity
 tf_regular <- function(TF, TF_ext_data){
   filter(TF_ext_data, type==TF & ext==TF)[[1,1]]
 }
@@ -51,24 +51,26 @@ tf_ext <- function(TF, TF_ext_data){
   filter(TF_ext_data, type==TF & ext=="ext")[[1,1]]
 }
 
+# NODE: TF_and_ext is a dataframe (loaded already) that created in order to identify whether the TF data
+# is a regular TF (with high confidence annotation) or ext type(with lower confidence)
+# Dogma: if we have regular TF type, we use that data; if we only have ext data, use ext
+# if we have no data related to this tf, we will either give an error or do nothing
 create_activity_data <- function(tf){ 
-  TF_activity_cell_tibble <- as_tibble(TF_active)
-  TF_ext_data <- TF_and_ext(TF_activity_cell_tibble)
   # use the feature of feather data to read certain col to optimize speed
   cell_col <- read_feather("data/joint_cortex/joint_cortex.regulon_activity_per_cell.feather",
                            "Cell")
-  # note that the first
+  # add certain tf activity data to the Cell column
   activity_cell <- cell_col
-  for(TF in tf){
+  for(TF in tf){ # tf is input tf list, could contain many tfs
     tf_to_read <- TF
     if(has_regular(TF, TF_ext_data)){
-      tf_to_read <- tf_regular(TF,TF_ext_data)
+      tf_to_read <- tf_regular(TF,TF_ext_data) # a helper to read the corresponding data
     }
     else if(has_ext(TF, TF_ext_data)){
       tf_to_read <- tf_ext(TF,TF_ext_data)
     }
     else{
-      next
+      next # means we don't have that data, we jump over it and do thing
     }
     col <- read_feather("data/joint_cortex/joint_cortex.regulon_activity_per_cell.feather",
                         tf_to_read)
@@ -138,6 +140,14 @@ plot_heatmap <- function(metadata, TF_active, activity_cluster){
 # function to create network
 
 
+#' Title
+#'
+#' @param tf 
+#'
+#' @return
+#' @export
+#'
+#' @examples
 create_network <- function(tf){ 
   # takes a vector input that contains user selected TFs
   # good to visualize correlations among multiple TFs
@@ -160,38 +170,127 @@ create_network <- function(tf){
   
   nodeData <- nodeData %>%
     mutate(color = case_when(id %in% tf ~ "#9d4097",
+                             id %in% unique_TF ~ "#D6604D",
                              id %in% mutual_target ~ "#4fafc6",
                              TRUE ~ "lightgrey"))
   return(list(nodes = nodeData,
               edges = edgeData
               ))
 }
-tf <- c("Arx","Lef1")
-
-nodeData <- create_network(tf)$nodes
-edgeData <- create_network(tf)$edges
-network <- createCytoscapeJsNetwork(nodeData, edgeData)
-rcytoscapejs2(network$nodes, network$edges)
 
 
 
-network_data <- create_network(c("Arx","Lef1"))
 
-nodeData <- network_data$nodes
-edgeData <- network_data$edges
+# make_cell_metadata
+create_cell_metadata <- function(metadata_part){
+  metadata_part %>% 
+    select(Age = orig.ident, Cell, Cluster = ID_20190730_with_blacklist_and_refined) %>% 
+    # In this case, we remove the "prefix" of the Cluster column, so that we are
+    # simply left with the abbreviation representing the cell type, so that 
+    # we can link the cells of the same cell type across ages
+    separate(Cluster, into = c("Prefix", "Cluster"), sep = "_") %>% 
+    mutate(Age = factor(Age, levels = c("Forebrain E12.5",
+                                        "Forebrain E15.5",
+                                        "Forebrain P0",
+                                        "Forebrain P3",
+                                        "Forebrain P6"))) %>% 
+    arrange(Cell)
+  
+}
+#cell_metadata_cortex <- create_cell_metadata(cell_metadata_cortex)
 
-unique_edges <- edgeData %>%
-    count(target) %>%
-    filter(n > 1 & !target %in% c("Arx","Dlx1") ) %>%
-  .[[1]]
 
-nodeData <- nodeData %>%
-  mutate(color = case_when(id %in% c("Arx","Lef1") ~ "#9d4097",
-                           id %in% unique_edges ~ "#4fafc6",
-                           TRUE ~ "lightgrey"))
+# since we have two datasets with different tf name, with ext and weights or not
+# and those tf names are essential for matching data, this function use a clean tf
+# and return a tf with ext and weight type
+translate_tf <- function(tf, tf_dataframe){
+  identify_tf(tf_dataframe) %>%
+    filter(type==tf) %>%
+    .[[1,1]]
+}
+tf_df <- as_tibble(rownames(activity))
+  
 
-network <- createCytoscapeJsNetwork(nodeData, edgeData)
+TF <- "Arx_extended (21g)" # input_tf
 
-rcytoscapejs2(network$nodes, network$edges)
+#activity <- readRDS("data/joint_cortex/joint_cortex.binaryRegulonActivity_nonDupl.Rds")
 
+#plot_timeseries(TF,cell_metadata_cortex,activity)
+
+plot_timeseries <- function(TF,cell_metadata, activity){
+  activity <- activity[TF, ] %>% 
+    {data.frame("TF" = .)} %>% 
+    tibble::rownames_to_column(var = "Cell") %>% # the original activity vector has names
+    arrange(Cell)
+  
+  if(!all(cell_metadata$Cell == activity$Cell)) return (-1)
+  # Add the TF activity to the new dataframe
+  ribbon_df <- cell_metadata
+  ribbon_df$TF <- activity$TF
+  
+  ribbon_df <- ribbon_df %>% 
+    filter(!grepl("BLACKLIST", Cluster))
+  ribbon_df_celltype_frac <- ribbon_df %>% 
+    group_by(Age) %>% 
+    # Total cells at each age
+    mutate(total = n()) %>% 
+    group_by(Age, Cluster) %>%
+    # Proportion of TF+ cells per cluster, per age
+    mutate(frac = sum(TF > 0) / total) %>% 
+    distinct(Age, Cluster, frac) %>% 
+    ungroup()
+  
+  ribbon_df_cum_frac <- ribbon_df %>% 
+    group_by(Age) %>% 
+    summarize(cumfrac = sum(TF > 0) / n()) %>% 
+    ungroup()
+  
+  timepoints2 <- ribbon_df$Age
+  clusters <- ribbon_df$Cluster
+  
+  df = data.frame(cluster = rep(unique(clusters), length(unique(timepoints2))),
+                  stage = do.call(c, lapply(as.character(unique(timepoints2)), rep, times = length(unique(clusters)))))
+  
+  df$ranking = match(df$cluster, names(colours))
+  df = df[order(df$stage, df$ranking),]
+  
+  df <- left_join(df, select(ribbon_df_celltype_frac, cluster = Cluster, stage = Age, frac)) %>% 
+    mutate(frac = replace_na(frac, 0)) %>% 
+    left_join(select(ribbon_df_cum_frac, stage = Age, cumfrac))
+  
+  df$xpos = match(df$stage, unique(timepoints2))
+  
+  df %>%
+    ggplot(aes(x = xpos, y = frac, fill = cluster)) +
+    geom_area(stat = "identity") +
+    scale_fill_manual(values = colour_palette, drop = FALSE, name = "") +
+    scale_x_continuous(breaks = seq_along(unique(df$stage)),
+                       labels = c("E12.5", "E15.5", "P0", "P3", "P6"),
+                       limits = c(1, length(unique(df$stage)))) +
+    labs(x = "age", y = "Binary activity", title = TF) +
+    guides(fill = guide_legend(ncol = 5)) +
+    theme(legend.position = "bottom")
+  
+}
+
+
+
+
+
+TF_tbl1 <- as_tibble(TF_active)
+TF_tb2<- as_tibble(tf_list)
+
+missing_tf <- TF_tbl1 %>%
+  mutate(exist = if_else(value %in% tf_list, 1, 0)) %>%
+  filter(exist==0)
+
+mutual_tf <- TF_tbl1 %>%
+  mutate(exist = if_else(value %in% tf_list, 1, 0)) %>%
+  filter(exist==1)
+  
+  
+  
+  
+  
+  
 
