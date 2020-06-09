2f5324232c2d682e1d5577c9836dae89358e6113
Anthony Ma <tonyma_0706@163.com>
2020-06-09 22:33:09 +0800

    Added Roxygen skeleton descriptions of each function

--- functions.R
+++ functions.R
@@ -1,29 +1,107 @@
-plotByValue <- function(tf, cluster_data){
+
+# --------------------------Tab1-cytoscape network visualization----------------------------------------------
+# function to create network
+
+
+#' Create rcytoscape network data
+#' 
+#' Takes a vector input that contains user selected TFs and output a list of nodeData and edgeData
+#' which will be used for createCytoscapeJsNetwork
+#' A good to visualize correlations among multiple TFs
+#' 
+#' @param tf one single tf name character
+#'
+#' @return a list of nodeData and edgeData that are required for generating a rcytoscapejs network object
+#' 
+#' @examples 
+#' TF <- c("Arx","Lef1")
+#' nodeData <- create_network(TF)$nodes
+#' edgeData <- create_network(TF)$edges
+#' network <- createCytoscapeJsNetwork(nodeData, edgeData)
+#' rcytoscapejs2(network$nodes, network$edges)
+#' 
+create_network <- function(tf){ 
+  TF_interest <- filter(TF_target_gene, TF %in% tf)[["TF"]]
+  gene_target <- filter(TF_target_gene, TF %in% tf)[["gene"]]
   
-  # because the data name is not good(contain space), 
-  # we firstly make a vector of that then call it, pick the first one
-  TF_activity <- select(cluster_data, contains(tf))[[1]] # a vector, not a list
+  source <- TF_interest
+  target <- gene_target
   
-  #arrange(cluster_data, desc(tf_col)) # sort
-  cluster_name <- cluster_data[[1]]
+  id <- c(TF_interest, gene_target)
+  name <- id
+  nodeData <- data.frame(id,name, stringsAsFactors = FALSE)
+  edgeData <- data.frame(source, target, stringsAsFactors = FALSE)
   
-  ggplot(data = cluster_data)+
-    geom_point(mapping = aes(x = cluster_name, y = TF_activity, color = cluster_name))+
-    theme(axis.text.x= element_text(angle = 90, hjust = 1))
+  mutual_target <- edgeData %>% 
+    # a character vector that indicates the nodes that are target of multiple selected TFs
+    count(target) %>%
+    filter(n > 1 & !target %in% tf ) %>%
+    .[[1]]
   
+  nodeData <- nodeData %>%
+    mutate(color = case_when(id %in% tf ~ "#9d4097",
+                             id %in% unique_TF ~ "#D6604D",
+                             id %in% mutual_target ~ "#4fafc6",
+                             TRUE ~ "lightgrey")) %>%
+    mutate(height = case_when(id %in% tf ~ "100",
+                           TRUE ~ "70")) %>%
+    mutate(width = case_when(id %in% tf ~ "100",
+                            TRUE ~ "70"))
+  return(list(nodes = nodeData,
+              edges = edgeData
+  ))
 }
-# generate a tibble that has two columns indicating whether the tf has ext type,
-# the ext column labels whether that data is ext type
+# ------------------------------------------------------------------------------------
+#' Identify transcription factor data type
+#' 
+#' Generate a tibble that has two columns indicating whether the tf has ext type,
+#' the ext column labels whether that data is ext type
+#'
+#' @param TF_name_activity_tibble a dataframe/tibble that saves all names of transcription 
+#' factor with suffix ext and weights
+#' E2f1_extended (133g) and E2f1 (122g) are two examples of tf data detected
+#' tf with no '_extended' attached on are those with high confidence annotation
+#' tf with extended are those with relatively low confidence annotation
+#' The point is to use the high annotation data if we have it, if not, we use
+#' the ext type tf data
+#' 
+#' @return a tibble/dataframe with columns 'type' and 'ext'
+#' type shows the tf name without any suffix -- ex. "E2f1"
+#' ext shows whether that data is ext/regular
+#' regular tf has its own name while extended data has 'ext' in that column
+#' This feature is important in following functions, has_regular, tf_regular ...
+#' to help retrieve the correct TF name to be used in other dataframes(tf activity in cell..)
+#' 
+#' @examples 
+#' TF_active <- as_tibble(read_rds("data/joint_cortex/joint_cortex.active_regulons.Rds"))
+#' TF_ext_data <- identify_tf(TF_active)
+#' 
 identify_tf <- function(TF_name_activity_tibble){
   TF_name_activity_tibble %>% 
     rename(name = value) %>%
-    mutate(no_space = str_replace_all(name, " \\(.+\\)$", ""))%>% #remove space and the gram
+    mutate(no_space = str_replace_all(name, " \\(.+\\)$", ""))%>% #remove space and the weight
     mutate(TF_type = str_replace(no_space, "_extended","_ext")) %>%
     mutate(ext = str_replace(TF_type, ".+ext", "ext")) %>%
     mutate(type = str_replace(TF_type, "_ext", "")) %>%
     select(name, type, ext)
 }
-# boolean functions that determine the identity of TF data in the activity datasets
+
+#' has_regular, has_ext
+#' 
+#' Boolean functions that determine the identity of TF data in the activity datasets
+#'
+#' @param TF character vector, containing one or more TF names
+#' @param TF_ext_data The dataframe/tibble generated using identify_tf, that has three cols:
+#' name, type, ext
+#'
+#' @return A boolean that checks if the datasets has the regular/ext data of that tf input
+#'
+#' @examples
+#' 
+#' has_regular("Arx", TF_ext_data) # False
+#' has_ext("Arx", TF_ext_data) # True
+#' 
+#' 
 has_regular <- function(TF, TF_ext_data){
   has_regular <- filter(TF_ext_data, type==TF & ext==TF)
   nrow(has_regular)!=0 #boolean
@@ -32,18 +110,49 @@ has_ext <- function(TF, TF_ext_data){
   is_ext <- filter(TF_ext_data, type==TF & ext=="ext")
   nrow(is_ext)!=0
 }
+
+#' tf_exist
+#' 
+#' This function also uses TF_ext_data, loaded in data_prep.R, can also generate using identify_tf
+#' 
+#' @param TF character vector, containing one or more TF names
+#'
+#' @return if the dataset doesn't have data for a tf input, it returns the tf input name; if it has them
+#' all, this returns TRUE
+#'
+#' @examples 
+#' TF_active <- as_tibble(read_rds("data/joint_cortex/joint_cortex.active_regulons.Rds"))
+#' TF_ext_data <- identify_tf(TF_active)
+#' TF <- "Arx"
+#' tf_exist(TF)
+#' 
+#' 
 tf_exist <- function(TF){
   for(tf in TF){
-    if(has_regular(tf, TF_ext_data) || has_ext(tf, TF_ext_data)){
-    }
-    else{
-      return (tf)
-    }
+    if(has_regular(tf, TF_ext_data) || has_ext(tf, TF_ext_data)){}
+    else{return (tf)}
   }
   return (TRUE)
-  #!has_regular(TF, TF_ext_data) && !has_ext(TF, TF_ext_data)
 }
+
 # read the corresponding data by tf's identity
+
+#' tf_regular, tf_ext
+#'
+#' @param TF character vector, containing one or more TF names
+#' @param TF_ext_data The dataframe/tibble generated using identify_tf, that has three cols:
+#' name, type, ext
+#'
+#' @return The best represented tf with suffix, still using the same logic: 
+#' Use the high annotation data if we have it, if not, we use the ext type tf data
+#' 
+#'
+#' @examples
+#' TF_active <- as_tibble(read_rds("data/joint_cortex/joint_cortex.active_regulons.Rds"))
+#' TF_ext_data <- identify_tf(TF_active)
+#' TF <- c("Arx","Lef1")
+#' tf_regular(TF, TF_ext_data)
+#' 
 tf_regular <- function(TF, TF_ext_data){
   filter(TF_ext_data, type==TF & ext==TF)[[1,1]]
 }
@@ -51,10 +160,24 @@ tf_ext <- function(TF, TF_ext_data){
   filter(TF_ext_data, type==TF & ext=="ext")[[1,1]]
 }
 
-# NODE: TF_and_ext is a dataframe (loaded already) that created in order to identify whether the TF data
-# is a regular TF (with high confidence annotation) or ext type(with lower confidence)
+# --------------------------------Tab2 data--------------------------------
+# NOTE: TF_and_ext is a dataframe (loaded already) that created in order to identify 
+# whether the TF data is a regular TF (with high confidence annotation) 
+# or ext type(with lower confidence)
 # Dogma: if we have regular TF type, we use that data; if we only have ext data, use ext
-# if we have no data related to this tf, we will either give an error or do nothing
+# if we have no data related to this tf, we will either give an error message or do nothing
+
+#' create activity data
+#'
+#' @param tf character vector, containing one or more TF names
+#'
+#' @return a dataframe that has a column containing all the cell names and columns of the input tfs
+#' the corresponding activity
+#' data value (NES) 
+#'
+#' @examples
+#' create_activity_data("Arx")
+#' 
 create_activity_data <- function(tf){ 
   # use the feature of feather data to read certain col to optimize speed
   cell_col <- read_feather("data/joint_cortex/joint_cortex.regulon_activity_per_cell.feather",
@@ -89,7 +212,6 @@ create_activity_data <- function(tf){
 makePheatmapAnno <- function(palette, column) {
   
   palette <- palette[unique(names(palette))]
-  
   # Make dataframe, retrieve the data frame
   anno_row <- data.frame(cluster = names(palette))
   names(anno_row) <- column
@@ -136,52 +258,17 @@ plot_heatmap <- function(metadata, TF_active, activity_cluster){
 } 
 
 
-# -------------------------------------cytoscape----------------------------------------------
-# function to create network
-
-
-#' Title
+#' make cell metadata of certain region, cortex/pon
 #'
-#' @param tf 
+#' @param metadata_part a dataframe, the cell_metadata corresponding to a region: cortex/pon
 #'
-#' @return
-#' @export
+#' @return a dataframe/tibble of metadata with columns Age, Cell, Prefix and Cluster, used for plotting
+#' timeseries
 #'
 #' @examples
-create_network <- function(tf){ 
-  # takes a vector input that contains user selected TFs
-  # good to visualize correlations among multiple TFs
-  TF_interest <- filter(TF_target_gene, TF %in% tf)[["TF"]]
-  gene_target <- filter(TF_target_gene, TF %in% tf)[["gene"]]
-  
-  source <- TF_interest
-  target <- gene_target
-  
-  id <- c(TF_interest, gene_target)
-  name <- id
-  nodeData <- data.frame(id,name, stringsAsFactors = FALSE)
-  edgeData <- data.frame(source, target, stringsAsFactors = FALSE)
-  
-  mutual_target <- edgeData %>% 
-    # a character vector that indicates the nodes that are target of multiple selected TFs
-    count(target) %>%
-    filter(n > 1 & !target %in% tf ) %>%
-    .[[1]]
-  
-  nodeData <- nodeData %>%
-    mutate(color = case_when(id %in% tf ~ "#9d4097",
-                             id %in% unique_TF ~ "#D6604D",
-                             id %in% mutual_target ~ "#4fafc6",
-                             TRUE ~ "lightgrey"))
-  return(list(nodes = nodeData,
-              edges = edgeData
-              ))
-}
-
-
-
-
-# make_cell_metadata
+#' cell_metadata_cortex <- read_tsv("data/joint_cortex/joint_cortex.metadata.tsv")
+#' cell_metadata_cortex <- create_cell_metadata(cell_metadata_cortex)
+#' 
 create_cell_metadata <- function(metadata_part){
   metadata_part %>% 
     select(Age = orig.ident, Cell, Cluster = ID_20190730_with_blacklist_and_refined) %>% 
@@ -197,29 +284,55 @@ create_cell_metadata <- function(metadata_part){
     arrange(Cell)
   
 }
-#cell_metadata_cortex <- create_cell_metadata(cell_metadata_cortex)
 
 
-# since we have two datasets with different tf name, with ext and weights or not
-# and those tf names are essential for matching data, this function use a clean tf
-# and return a tf with ext and weight type
+#' Translate transcription factor name version
+#' 
+#' Since we have two datasets with different tf name format, (with ext and weights or not)
+#' and those tf names are essential for retrieving data in various datasets,
+#' this function use a clean tf and return a tf with ext and weight type
+#'
+#' @param tf a character vector of a tf without any suffix. EX: "Arx" 
+#' @param tf_dataframe a one column dataframe that contains all the TF names with suffix
+#' 
+#' @return If the dataframe contains the tf input, it return a best represented tf name
+#'  with ext and weight suffix. If not, it returns FALSE
+#'
+#' @examples
+#' tf_df <- as_tibble(rownames(activity))
+#' translate_tf("Arx",tf_df)  # Arx_extended (21g)
+#' translate_tf("Brahl",tf_df) # FALSE
+#' 
 translate_tf <- function(tf, tf_dataframe){
-  tf_info <- identify_tf(tf_dataframe) %>%
-    filter(type==tf) #%>%
-    #if_else(nrow(.)!=0,.[[1,1]], FALSE)
-  if(nrow(tf_info)!=0) tf_info[[1,1]]
-  else{return (FALSE)}
+  tf_info <- identify_tf(tf_dataframe)
+  if(has_regular(tf, tf_info)){
+    tf_regular(tf,tf_info) # a helper to read the corresponding data
+  }
+  else if(has_ext(tf, tf_info)){
+    tf_ext(tf,tf_info)
+  }
+  else{
+    return (FALSE) # means we don't have that data
+  }
 }
-tf_df <- as_tibble(rownames(activity))
 
-translate_tf("Barhl2",tf_df)
-
-TF <- "Arx_extended (21g)" # input_tf
-
-#activity <- readRDS("data/joint_cortex/joint_cortex.binaryRegulonActivity_nonDupl.Rds")
-
-#plot_timeseries(TF,cell_metadata_cortex,activity)
-
+#' Plot timeseries
+#'
+#' @param TF a character vector that contains one or multiple TF names
+#' @param cell_metadata cell_metadata_cortex, loaded in data_prep.R, a dataframe
+#' with Age, Cell, Prefix, Cluster columns, this data is specific to forebrain cells/pon cells
+#' @param activity loaded in data_prep.R
+#'
+#' @return a plot that displays the percentage level of that tf along with several the time points
+#'
+#' @examples
+#' tf_df <- as_tibble(rownames(activity))
+#' TF <- translate_tf("Lef1",tf_df)
+#' binary_activity <- readRDS("data/joint_cortex/joint_cortex.binaryRegulonActivity_nonDupl.Rds")
+#' cell_metadata_cortex <- read_tsv("data/joint_cortex/joint_cortex.metadata.tsv")
+#' cell_metadata_cortex <- create_cell_metadata(cell_metadata_cortex)
+#' plot_timeseries(TF,cell_metadata_cortex, binary_activity)
+#' 
 plot_timeseries <- function(TF,cell_metadata, activity){
   activity <- activity[TF, ] %>% 
     {data.frame("TF" = .)} %>% 
@@ -273,24 +386,22 @@ plot_timeseries <- function(TF,cell_metadata, activity){
     labs(x = "age", y = "Binary activity", title = TF) +
     guides(fill = guide_legend(ncol = 5)) +
     theme(legend.position = "bottom")
-  
 }
 
 
 
 
 
-TF_tbl1 <- as_tibble(TF_active)
-TF_tb2<- as_tibble(tf_list)
+#TF_tbl1 <- as_tibble(TF_active)
+#TF_tb2<- as_tibble(tf_list)
 
-missing_tf <- TF_tbl1 %>%
-  mutate(exist = if_else(value %in% tf_list, 1, 0)) %>%
-  filter(exist==0)
-
-mutual_tf <- TF_tbl1 %>%
-  mutate(exist = if_else(value %in% tf_list, 1, 0)) %>%
-  filter(exist==1)
-  
+# missing_tf <- TF_tbl1 %>%
+#   mutate(exist = if_else(value %in% tf_list, 1, 0)) %>%
+#   filter(exist==0)
+#
+# mutual_tf <- TF_tbl1 %>%
+#   mutate(exist = if_else(value %in% tf_list, 1, 0)) %>%
+#   filter(exist==1)
   
   
   
