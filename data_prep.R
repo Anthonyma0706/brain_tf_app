e08ea1e2cbe11836b77de1a4ee620c27d1137587
Anthony Ma <tonyma_0706@163.com>
2020-06-24 23:07:13 +0800

    remove datasets reading from app.R and add that into data_prep.R

--- data_prep.R
+++ data_prep.R
@@ -4,13 +4,20 @@ library(readr)
 forebrain_data <- read_tsv("data/joint_cortex/Forebrain_join.2D.tsv") # for UMAP cluster
 TF_active <- as_tibble(read_rds("data/joint_cortex/joint_cortex.active_regulons.Rds"))
 
-TF_and_ext <- TF_active %>% 
-  rename(name = value) %>%
-  mutate(no_space = str_replace_all(name, " \\(.+\\)$", ""))%>% #remove space and the gram
-  mutate(TF_type = str_replace(no_space, "_extended","_ext")) %>%
-  mutate(ext = str_replace(TF_type, ".+ext", "ext")) %>%
-  mutate(type = str_replace(TF_type, "_ext", "")) %>%
-  select(name, type, ext)
+# These datasets describe TF and genes that are target of TFs, don't have ext suffix
+TF_target_gene <- as_tibble(read_rds("data/joint_cortex/joint_cortex.regulon_target_info.Rds")) %>%
+  select(-logo)
+unique_TF <- unique(TF_target_gene[["TF"]])
+
+
+TF_and_ext <- identify_tf(TF_active)
+# TF_and_ext <- TF_active %>% 
+#   rename(name = value) %>%
+#   mutate(no_space = str_replace_all(name, " \\(.+\\)$", ""))%>% #remove space and the gram
+#   mutate(TF_type = str_replace(no_space, "_extended","_ext")) %>%
+#   mutate(ext = str_replace(TF_type, ".+ext", "ext")) %>%
+#   mutate(type = str_replace(TF_type, "_ext", "")) %>%
+#   select(name, type, ext)
 
 # make color palette
 metadata <- read_tsv("data/joint_cortex/metadata_20190716.tsv")
@@ -34,7 +41,7 @@ binary_activity <- readRDS("data/joint_cortex/joint_cortex.binaryRegulonActivity
 tf_df <- as_tibble(rownames(binary_activity)) #a dataframe that contains all the tf with 
 # best representation of its identity in the binary_activity dataset
 
-save(forebrain_data,TF_and_ext,TF_active,metadata,tf_df,
+save(forebrain_data,TF_and_ext,TF_active,metadata,tf_df,TF_target_gene, unique_TF,
      cell_metadata_cortex,binary_activity, file = "data/joint_cortex/cortex_prep.Rda")
 
 
