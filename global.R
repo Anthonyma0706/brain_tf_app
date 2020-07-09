c7da9761d431ee26880e70dc1d5b589590310d32
sjessa <selinjessa@gmail.com>
2020-07-09 11:39:47 -0400

    Update libraries to not use tidyverse

--- global.R
+++ global.R
@@ -1,14 +1,21 @@
+# Libraries
 library(shiny)
-library(tidyverse)
+library(tidyr)
+library(stringr)
+library(tibble)
 library(dplyr)
 library(feather)
 library(plotly)
 library(ggplot2)
 library(pheatmap)
 library(DT)
-library(rcytoscapejs2)
+library(rcytoscapejs2) # downloaded from https://github.com/uc-bd2k/rcytoscapejs2
 library(glue)
+
+# Data
 load("data/joint_cortex/cortex_prep.Rda") # a list, data_cortex
 load("data/joint_pons/pons_prep.Rda")     # a list, data_pons
 load("data/joint_cortex/common_prep.Rda") # metadata and colour_palettes
+
+# Custom functions
 source("functions.R")
